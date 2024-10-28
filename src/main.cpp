76a5cec42c02a1b145b9cfff026b928b53bd0bff
RafalJakimowicz <bobix7913@gmail.com>
2024-10-28 13:14:53 +0100

    main

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include "BluetoothA2DPSink.h"
+#define PWM_PIN 2
+
+
+BluetoothA2DPSink a2dp_sink;
+TaskHandle_t taskOnCore0;
+int16_t *values;
+uint32_t len;
+int absmax = 1024;
+void read_data_stream(const uint8_t *data, uint32_t length) {
+    // Print audio data to serial
+    len = length/2;
+    values = (int16_t*) data;
+    for (int j=0; j<length/2; j++){
+      //Serial.print(values[j]);
+      //Serial.print(" ");
+    }
+    //Serial.print("\n");
+}
+
+void startBTRecieve(void * tParameters){
+  Serial.print("Start recieving on core: ");
+  Serial.println(xPortGetCoreID());
+  // output to callback and no I2S 
+  a2dp_sink.set_stream_reader(read_data_stream, false);
+  // connect to MyMusic with no automatic reconnect
+  a2dp_sink.start("ESP_32", false);  
+  for (;;){
+    delay(1000);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  //Create recieving task on core 0
+  xTaskCreatePinnedToCore(startBTRecieve, "recieveData", 20000, NULL, 0, &taskOnCore0, 0);
+}
+
+
+void loop() {
+  int16_t *valBuff = values;
+  for(int i = 0; i < len; i++){
+    int data = valBuff[i] + 1024;
+    if(data != 1024){
+      float percent = (float)data / 2048.0000;
+      int dutyCycle = (int)255.00*percent;
+      Serial.println(data);
+      analogWrite(PWM_PIN, dutyCycle);
+    }
+    else{
+      analogWrite(PWM_PIN, 0);
+    }
+  }
+}
