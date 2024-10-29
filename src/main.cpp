e445728d141ccadf4a874be9b222a75770a14324
RafalJakimowicz <bobix7913@gmail.com>
2024-10-29 10:54:51 +0100

    add mutex

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
 
 
 BluetoothA2DPSink a2dp_sink;
-TaskHandle_t taskOnCore0;
+SemaphoreHandle_t mutex;
 int16_t *values;
 uint32_t len;
 int absmax = 1024;
 void read_data_stream(const uint8_t *data, uint32_t length) {
+  if(xSemaphoreTake(mutex,portMAX_DELAY)){
     len = length/2;
     values = (int16_t*) data;
+    xSemaphoreGive(mutex);
+  }
 }
 
 void startBTRecieve(void * tParameters){
@@ -25,15 +28,14 @@ void startBTRecieve(void * tParameters){
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  //Create recieving task on core 0
-  xTaskCreatePinnedToCore(startBTRecieve, "recieveData", 20000, NULL, 0, &taskOnCore0, 0);
-}
-
-
-void loop() {
-  int16_t *valBuff = values;
+void CreatePWMFromData(void * tParameters){
+  int16_t *valBuff;
+  while(true){
+    if(xSemaphoreTake(mutex, portMAX_DELAY)){
+      valBuff = values;
+      xSemaphoreGive(mutex);
+    }
+  }
   for(int i = 0; i < len; i++){
     int data = valBuff[i] + 1024;
     if(data != 1024){
@@ -48,4 +50,17 @@ void loop() {
       analogWrite(PWM_PIN, 0);
     }
   }
+}
+
+void setup() {
+  Serial.begin(115200);
+  //Create recieving task on core 0
+  mutex = xSemaphoreCreateMutex();
+  xTaskCreatePinnedToCore(startBTRecieve, "recieveData", 20000, NULL, 0, NULL, 0);
+  xTaskCreatePinnedToCore(CreatePWMFromData, "PWMData", 20000, NULL, 0, NULL, 0);
+}
+
+
+void loop() {
+ //-----------
 }
